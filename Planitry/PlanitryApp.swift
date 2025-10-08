9b69e49ae5942e9ba0cdbfa2d6404aa884b3472a
Mamie Gartin <megartin@student.fullsail.edu>
2025-10-07 20:24:17 -0400

    Initial Commit

---
+++ Planitry/PlanitryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanitryApp.swift
+//  Planitry
+//
+//  Created by Mamie Gartin on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PlanitryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlanitryTests/PlanitryTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlanitryTests.swift
+//  PlanitryTests
+//
+//  Created by Mamie Gartin on 10/7/25.
+//
+
+import Testing
+@testable import Planitry
+
+struct PlanitryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
